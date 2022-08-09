88e671d8bf4e56c97aef54db58c1693ed3629fb2
Daniel Toloto <dctoloto@gmail.com>
2022-08-09 20:20:23 -0300

    fix api url for demo

--- src/services/api/getData/index.ts
+++ src/services/api/getData/index.ts
@@ -12,7 +12,14 @@ export const getData = async () => {
   // local data
   // await new Promise(resolve => setTimeout(resolve, 2000))
   // return countryData
-  const response = await fetch('http://universities.hipolabs.com/search?country=United+States')
+
+  // THIS IS BEING IMPLEMENTED FOR DEMO PURPOSE, 
+  // SINCE UNIVERSITIES API IS NOT USING SSL CONNECTION
+  const API_URL = process.env.NODE_ENV === 'production' 
+    ? 'https://auto-complete-component-six.vercel.app/data.json' 
+    : 'http://universities.hipolabs.com/search?country=United+States'
+
+  const response = await fetch(API_URL)
   const data = await response?.json()
   const universityNames: string[] = data?.map((item: any) => item?.name )
   return universityNames
